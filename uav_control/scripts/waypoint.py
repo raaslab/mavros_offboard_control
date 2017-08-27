bcf0b1e195bfd9ae09b35397b99a68393943e1e8
Kevin Li Yu <klyu@vt.edu>
2017-08-27 17:38:02 -0400

    updated waypoint.py/still isn't able to take off autonomously

--- uav_control/scripts/waypoint.py
+++ uav_control/scripts/waypoint.py
@@ -7,81 +7,63 @@ from mavros_msgs.msg import *
 from mavros_msgs.srv import *
 from std_msgs.msg import String
 
-#def talker():
-#    pub = rospy.Publisher('chatter', String, queue_size=10)
-#    rospy.init_node('talker', anonymous=True)
-#    rate = rospy.Rate(10) # 10hz
-#    while not rospy.is_shutdown():
-#        hello_str = "hello world %s" % rospy.get_time()
-#        rospy.loginfo(hello_str)
-#        pub.publish(hello_str)
-#        rate.sleep()
-#
-#if __name__ == '__main__':
-#    try:
-#        talker()
-#    except rospy.ROSInterruptException:
-#        pass
-
-def handle_waypoints(data):
+def waypoint_callback(data):
+	print("\nwaypoint_callback")
 	rospy.loginfo("Got waypoint: %s", data)
 
-def callback():
-	rospy.loginfo("Got waypoints: %s", data)
-
-	
 def main():
 	rospy.init_node('wayPoint')
-	rospy.Subscriber("/mavros/mission/waypoints", WaypointList, callback)
+	rospy.Subscriber("/mavros/mission/waypoints", WaypointList, waypoint_callback)
+	
+	#Arming
+	print("\nArming")
+	rospy.wait_for_service("/mavros/cmd/arming")
+	rospy.loginfo("Arming UAV")
+	uav_arm = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
+	resp = uav_arm()
+	rospy.loginfo(resp)
+	rospy.sleep(5)
+	
+	#Takeoff
+	print("\nTakeoff")
+	rospy.wait_for_service("/mavros/cmd/takeoff")
+	rospy.loginfo("Takeoff UAV")
+	uav_takeoff = rospy.ServiceProsy("/mavros/cmd/takeoff", 
+	
+	#Clearing waypoints
+	print("\nClearing")
+	rospy.wait_for_service("/mavros/mission/clear")
+	rospy.loginfo("Clearing Waypoints")
+	waypoint_clear = rospy.ServiceProxy("/mavros/mission/clear", WaypointClear)
+	resp = waypoint_clear()
+	rospy.loginfo(resp)
+
+	#Sending waypoints_push
+	print("\nPushing")
 	rospy.loginfo("Waiting for MAVROS service...")
 	rospy.wait_for_service("/mavros/mission/push")
 	
 	waypoints = [
-		Waypoint(frame = 3, command = 24, is_current = True,
-				 x_lat = 44.57, y_long = -123.27, z_alt = 3.0),
-		Waypoint(frame = 3, command = 16, is_current = True,
-				 x_lat = 44.57, y_long = -123.27, z_alt = 3.0)
+		Waypoint(frame = 3, command = 22, is_current = True,
+				 x_lat = 44, y_long = -123, z_alt = 3.0),
 	]
 	
 	waypoint_push = rospy.ServiceProxy("/mavros/mission/push", WaypointPush)
 	resp = waypoint_push(waypoints)
 	rospy.loginfo(resp)
 	
-	
-#		wayPointInfo = rospy.Subscriber(mavros.get_topic('mission', 'waypoints'))
-#		rospy.loginfo(wayPointInfo)
-#		rate.sleep()
-#	rospy.Subscriber('/mavros/mission/waypoints ', wayPointList, logWayPoint)
-#	
-#	# pull waypoints
-#	rospy.loginfo("Waiting for Mavros service PULL...")
-#	rospy.wait_for_service('/mavros/mission/pull', timeout=None)
-#	rospy.ServiceProxy('/mavros/mission/pull', wayPointPull)
-#	rospy.loginfo("Pulled wayPoints %s", wayPointPull)
-#	
-#	# push waypoints
-#	rospy.loginfo("Waiting for Mavros service PUSH...")
-#	rospy.wait_for_service('/mavros/mission/push', timeout=None)
-#	
-#	waypoints = [
-#		Waypoint(frame=Waypoint.FRAME_GLOBAL_REL_ALT,
-#			command=Waypoint.NAV_WAYPOINT,
-#			is_current=True,
-#			x_lat=44.57, y_long=-123.27, z_alt=3.0)
-#		]
-#	rospy.ServiceProxy('/mavros/mission/push', wayPointPush)
-#	rospy.loginfo("Pushed wayPoints %s", wayPointPush(waypoints))
-#	
+	#Call waypoints_pull
+	print("\nPulling")
+	rospy.wait_for_service("/mavros/mission/pull")
+	rospy.loginfo("Calling Waypoint_pull Service")
+	waypoint_pull = rospy.ServiceProxy("/mavros/mission/pull", WaypointPull)
+	resp = waypoint_pull()
+	rospy.loginfo(resp)
+
+	rospy.spin()
 
 if __name__ == '__main__':
-	try:
-		main()
-	except rospy.ROSInterruptException:
-		pass
- 
-
-
-
+	main()
 
 # def  (data):
     # rospy.loginfo("Got waypoints: %s", data)
@@ -119,7 +101,3 @@ if __name__ == '__main__':
     # rospy.loginfo(resp)
 
     # rospy.spin()
-
-
-# if __name__ == '__main__':
-    # main()
