a4967e872d71076a41cf3846b7e1487cbc88a630
Kevin Li Yu <klyu@vt.edu>
2018-02-25 12:06:59 -0500

    replaced plotGPS with new plotter that exports to excel

--- uav_control/scripts/plotGPS.py
+++ uav_control/scripts/plotGPS.py
@@ -7,30 +7,24 @@ import yaml
 import utm
 import time
 import matplotlib.pyplot as plt
+import xlsxwriter
+from mpl_toolkits.basemap import Basemap
 from mavros_msgs.msg import *
 from mavros_msgs.srv import *
-from std_msgs.msg import String
-from std_msgs.msg import Int64
+from std_msgs.msg import String, Int64
 from sensor_msgs.msg import NavSatFix
 
-#global variables
-latitude = 0.0
-longitude = 0.0
-altitude = 0.0
-last_waypoint = False
-tolerance = 0.00015
-ugv_ready = "0"
+latitude = 37.0
+longitude = -80.0
+
 
 def waypoint_callback(data):
-	print("\n----------waypoint_callback----------")
 	global last_waypoint
-	rospy.loginfo("Got waypoint: %s", data)
-	if len(data.waypoints) != 0:							# If waypoint list is not empty
-		rospy.loginfo("is_current: %s", data.waypoints[len(data.waypoints)-1].is_current)
-		last_waypoint = data.waypoints[len(data.waypoints)-1].is_current	# Checks status of "is_current" for last waypoint
+	if len(data.waypoints) !=0:
+		last_waypoint = data.waypoints[len_data.waypoints-1].is_current
+	return
 
 def globalPosition_callback(data):
-	# print("\n----------globalPosition_callback----------")
 	global latitude
 	global longitude
 	global altitude
@@ -38,64 +32,62 @@ def globalPosition_callback(data):
 	longitude = data.longitude
 	altitude = data.altitude
 
-def ready_callback(data):
-	# rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-	global ugv_ready
-	ugv_ready = data.data
-
-def pushingWaypoints(poi):
-	print("\n----------pushingWaypoints----------")
-	rospy.wait_for_service("/mavros/mission/push")
-	waypoint_push = rospy.ServiceProxy("/mavros/mission/push", WaypointPush)
-	resp = waypoint_push(0,poi)
-	rospy.sleep(5)
-	return
-
-def takeoff_call(lat, long, alt):
-	print("\n----------takeoff_call----------")
-	takeoff = rospy.ServiceProxy("/mavros/cmd/takeoff", CommandTOL)
-	resp = takeoff(0,0,lat,long, alt)
-	rospy.sleep(5)
-	return
-
-def plotData(xData, yData, axis):
-	print("plotData======================================")
+def plotData(xData, yData, axis, n):
+	# xData = [i for i in xData if i != xDel]
+	# yData = [i for i in yData if i != yDel]
+	xData = xData[n:]
+	yData = yData[n:]
 	plt.plot(xData, yData, 'ro')
 	if axis == 0:
 		return
 	else:
-		plt.axis(axis)
-	plt.show()
+		plot.axis(axis)
 
 def main():
 	rospy.init_node('gpsPlotter')
-	rospy.Subscriber("/mavros/mission/waypoints", WaypointList, waypoint_callback)
+	# rospy.Subscriber("/mavros/mission/waypoints", WaypointList, waypoint_callback)
 	rospy.Subscriber("/mavros/global_position/raw/fix", NavSatFix, globalPosition_callback)
-	# rospy.Subscriber("/mavros/ugv/ready", Int64, ready_callback)	
-	# readyBit = rospy.Publisher("/mavros/uav/ready", Int64, queue_size=10) # Flag topic
-	# readyBit.publish(0)
-	# clear_pull()
-	
-	xData = []
-	yData = []
+	rate = rospy.Rate(1)
+	xData = [0,0]
+	yData = [0,0]
 	counter = 0
-	while True:
-		[xDataTemp, yDataTemp, rando, rando1] = utm.from_latlon(latitude, longitude)
-		print(counter, "- xDataTemp:", xDataTemp, "yDataTemp:", yDataTemp)
-		xData.append(xDataTemp)
-		yData.append(yDataTemp)
-		time.sleep(1)
-		counter = counter + 1
-		if counter == 400:
-			print("Break")
-			break
+	totalTime = 460
+	resolution = 0.5
+	numDelPoints = 20	
+	while not rospy.is_shutdown():
+		try:
+			[x, y, rand, rand1] = utm.from_latlon(latitude, longitude)
+			print(counter, "- x: ", x, " latitude: ", latitude, " longitude: ", longitude, " y: ", y)
+			# print(len(xData))
+			# print(xData[-1])
+			if len(xData) > 1:
+				if x != xData[-1]:
+					xData.append(x)
+					yData.append(y)
+					# time.sleep(resolution)
+					counter = counter+1
+					# if counter == totalTime:
+					# 	print("break")
+					# 	break
+		except KeyboardInterrupt:
+			print("All done")
 
-	plotData(xData, yData, 0)
+	# [xDel, yDel, rand, rand1] = utm.from_latlon(0,0)
+	# map = Basemap(llcrnrlon=-85,llcrnrlat=30,urcrnrlon=-70,urcrnrlat=40,
+ #             resolution='f', projection='cyl')
+	# map.bluemarble()
+	# map.drawcoastlines()
+	workbook   = xlsxwriter.Workbook('data.xlsx')
+	worksheet1 = workbook.add_worksheet()
+	worksheet1.write_column(0,0,xData)
+	worksheet1.write_column(0,1,yData)
 
-	# DONE
+	# plt.show()
+	# print(xData)
+	plotData(xData, yData, 0, numDelPoints)
+	plt.show()
+	# time.sleep(1)
 	print("Finished")
-	rospy.spin()
-
 
 if __name__ == '__main__':
 	main()
